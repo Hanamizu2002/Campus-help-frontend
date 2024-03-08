8b2f9187bfd01861e2ffc19804858888318c32fd
Kyle Ma <m2069134445@icloud.com>
2024-03-08 17:36:51 +0800

    修改学校管理界面

--- src/views/admin/children/School.vue
+++ src/views/admin/children/School.vue
@@ -1,242 +1,212 @@
 <template>
-	<div class="content">
-		<el-table :data="dataList" style="width: 100%;margin-bottom: 20px;" row-key="uuid" border
-			:default-expand-all='false' :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-			<el-table-column prop="name" label="学校" sortable width="230">
-			</el-table-column>
-			<el-table-column prop="deptname" label="分院" sortable width="180">
-			</el-table-column>
-			<el-table-column prop="classesname" label="专业">
-			</el-table-column>
+  <div class="content">
+    <el-button @click="handleAddSchool" type="primary">添加学校</el-button>
+    <el-button @click="handleAddDept" type="primary">添加分院</el-button>
+    <el-button @click="handleAddClass" type="primary">添加专业</el-button>
 
-			<el-table-column label="操作">
-				<template slot-scope="scope">
-					<el-button icon="el-icon-plus" size="mini" @click="handleAdd(scope.$index, scope.row)">增加
-					</el-button>
-
-					<el-button icon="el-icon-edit" size="mini" @click="handleUpdate(scope.$index, scope.row)">修改
-					</el-button>
-
-					<el-button icon="el-icon-delete" size="mini" type="danger"
-						@click="handleDelete(scope.$index, scope.row)">删除
-					</el-button>
-				</template>
-			</el-table-column>
-
-			<!-- ------------新加入---------- -->
-
-
-			<!-- 添加或修改学生管理对话框 -->
-			<el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-				<el-form ref="form" :model="form" :rules="rules" label-width="80px">
-					<el-form-item label="学校" prop="name">
-						<el-input v-model="form.name" placeholder="请输入学校" />
-					</el-form-item>
-					<el-form-item label="分院" prop="deptname">
-						<el-input v-model="form.deptname" placeholder="请输入分院" />
-					</el-form-item>
-					<el-form-item label="专业" prop="classesname">
-						<el-input placeholder="请输入专业" v-model="form.classesname" />
-					</el-form-item>
-				</el-form>
-				<div slot="footer" class="dialog-footer">
-					<el-button type="primary" @click="submit()">确 定</el-button>
-					<el-button @click="cancel">取 消</el-button>
-				</div>
-			</el-dialog>
-		</el-table>
-	</div>
+    <el-dialog :title="dialogTitle" :visible.sync="dialogVisible" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="学校名称"
+                      v-if="dialogType === 'school' || dialogType === 'dept' || dialogType === 'class'">
+          <el-select v-model="form.schoolId" filterable placeholder="请选择学校"
+                     v-if="dialogType === 'dept' || dialogType === 'class'" @change="fetchDepts(form.schoolId)">
+            <el-option v-for="school in schools" :key="school.id" :label="school.name" :value="school.id"></el-option>
+          </el-select>
+          <el-input v-model="form.name" placeholder="请输入学校名称" v-if="dialogType === 'school'"></el-input>
+        </el-form-item>
+        <el-form-item label="分院名称" v-if=" dialogType === 'dept' || dialogType === 'class'">
+          <el-select v-model="form.deptId" filterable placeholder="请选择分院" v-if="dialogType === 'class'">
+            <el-option v-for="dept in deptBySchool" :key="dept.id" :label="dept.name" :value="dept.id"></el-option>
+          </el-select>
+          <el-input v-model="form.name" placeholder="请输入分院名称" v-if="dialogType === 'dept'"></el-input>
+        </el-form-item>
+        <el-form-item label="专业名称" v-if="dialogType === 'class'">
+          <el-input v-model="form.name" placeholder="请输入专业名称"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="submitForm()">确定</el-button>
+      </span>
+    </el-dialog>
+    <el-table :data="dataList" style="width: 100%;margin-bottom: 20px;" row-key="uuid" border
+              :default-expand-all='false' :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+      <el-table-column prop="name" label="学校" sortable width="230">
+      </el-table-column>
+      <el-table-column prop="deptname" label="分院" sortable width="180">
+      </el-table-column>
+      <el-table-column prop="classesname" label="专业">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button icon="el-icon-edit" size="mini" @click="handleUpdate(scope.$index, scope.row)">修改
+          </el-button>
+          <el-button icon="el-icon-delete" size="mini" type="danger"
+                     @click="handleDelete(scope.$index, scope.row)">删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: "School",
-		data() {
-			return {
-				value: null,
+export default {
+  name: "SchoolManagement",
+  data() {
+    return {
+      schools: [],
+      depts: [],
+      deptBySchool: ["请选择学校"],
+      dialogVisible: false,
+      dialogTitle: '',
+      dialogType: '', // 'school', 'dept', 'class'
+      form: {
+        schoolId: '',
+        deptId: '',
+        name: ''
+      },
+      rules: {
+        name: [{required: true, message: '请输入名称', trigger: 'blur'}]
+      },
+      a: '',
+    };
+  },
+  methods: {
+    guid2() {
+      function S4() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+      }
 
-				schools: [],
-				// 弹出层标题
-				title: "",
-				// 是否显示弹出层
-				open: false,
-				// 表单参数
-				form: {},
-				// 表单校验
-				rules: {},
-			}
-		},
-		methods: {
-			guid2() {
-				function S4() {
-					return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-				}
-				return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-			},
+      return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+    },
+    fetchSchools() {
+      this.$get('/school').then(response => {
+        this.schools = response.data.school;
+      });
+      this.a = '';
+    },
+    fetchDepts(id) {
+      this.$get('/dept/school/' + id).then(response => {
+        this.deptBySchool = response.data.dept;
+      });
+    },
+    handleAddSchool() {
+      this.dialogType = 'school';
+      this.dialogTitle = '添加学校';
+      this.form = {name: ''};
+      this.dialogVisible = true;
+      this.a = 'school';
+    },
+    handleAddDept() {
+      this.dialogType = 'dept';
+      this.dialogTitle = '添加分院';
+      this.form = {schoolId: '', name: ''};
+      this.dialogVisible = true;
+      this.fetchSchools();
+      this.a = 'dept';
+    },
+    handleAddClass() {
+      this.dialogType = 'class';
+      this.dialogTitle = '添加专业';
+      this.form = {schoolId: '', deptId: '', name: ''};
+      this.dialogVisible = true;
+      this.a = 'class';
+    },
+    submitForm() {
+      this.dialogVisible = false;
+      if (this.a === "school") {
+        this.$post('/school', this.form).then(
+            res => {
+              this.$notifyMsg("成功", res.data.msg, "success")
+              this.fetchSchools();
+            }
+        );
+      } else if (this.a === "dept") {
+        this.$post('/dept', this.form).then(
+            res => {
+              this.$notifyMsg("成功", res.data.msg, "success")
+              this.fetchSchools();
+            }
+        );
+      } else if (this.a === "class") {
+        this.$post('/class', this.form).then(
+            res => {
+              this.$notifyMsg("成功", res.data.msg, "success")
+              this.fetchSchools();
+            }
+        );
+      }
 
-			//-------新加入---------
-
-			/** 新增按钮操作 */
-			handleAdd(a, b) {
-				// console.log(b.children.length)
-				// console.log(b)
-				if (b.schoolId != null) {
-					this.value = b;
-					this.reset();
-					this.title = "添加学校信息";
-					this.open = true;
-				}
-			},
-
-			/** 提交按钮 */
-			submit() {
-				// console.log(this.value.children.[0])
-				this.$refs["form"].validate(valid => {
-					if (valid) {
-						if (this.form.id != null) {
-							updateStuage(this.form).then(response => {
-								this.$message("修改成功");
-								this.open = false;
-								this.newList();
-							});
-						} else {
-							this.$post("/school", {
-								"name": this.form.name,
-							}).then(response => {
-								this.$message("新增学校成功");
-								this.newList();
-								this.$post("/dept", {
-									"schoolId": this.value.schoolId,
-									"name": this.form.deptname,
-								}).then(response => {
-									const name = this.form.deptname;
-									for (let i = 0; i < this.value.children.length; i++) {
-										if (this.value.children[i].deptname === name) {
-											this.$message("新增专业成功");
-											this.newList();
-											this.$post("/class", {
-												"schoolId": this.value.schoolId,
-												"deptId": this.value.children[i].deptsId,
-												"name": this.form.classesname,
-											}).then(response => {
-												this.$message("新增班级成功");
-												this.newList();
-											});
-										}
-									}
-
-								});
-								this.$message("新增成功");
-								this.open = false;
-								this.newList();
-							});
-						}
-					}
-				});
-			},
-
-
-
-			// 取消按钮
-			cancel() {
-				this.open = false;
-			},
-
-			handleDelete(a, b) {
-				// console.log(b);
-				if (b.schoolId != null) {
-					this.$del("/school/" + b.schoolId)
-						.then((res) => {
-							this.$notifyMsg("成功", res.data.msg, "success")
-							this.newList()
-						})
-				} else if (b.deptsId != null) {
-					this.$del("/dept/" + b.deptsId)
-						.then((res) => {
-							this.$notifyMsg("成功", res.data.msg, "success")
-							this.newList()
-						})
-				} else {
-					this.$del("/class/" + b.classesId)
-						.then((res) => {
-							this.$notifyMsg("成功", res.data.msg, "success")
-							this.newList()
-						})
-				}
-			},
-
-
-			/** 修改按钮操作 */
-			handleUpdate(a, b) {
-				// console.log(b);
-				this.$get("/school/" + b.schoolId).then((res) => {
-					this.form = res.data.school;
-					this.open = true;
-					this.title = "修改学校管理";
-				});
-			},
-
-			newList() {
-				this.$get('/school')
-					.then(res => {
-						// console.log(res.data)
-						// console.log(this.schools)
-						this.schools = res.data.school
-					})
-			},
-
-			// 表单重置
-			reset() {
-				this.form = {
-					id: null,
-					name: null,
-					age: null
-				};
-				// this.resetForm("form");
-			},
-			//-------新加入---------
-		},
-
-		created() {
-			this.newList();
-		},
-
-		computed: {
-			dataList() {
-				return this.schools.map(item => {
-					let depts = [];
-					for (let i = 0; i < item['depts'].length; i++) {
-						// console.log(item['depts'][i]['classes'])
-						depts.push({
-							deptsId: item['depts'][i].id,
-							deptname: item['depts'][i].name,
-							uuid: this.guid2()
-						})
-						// console.log(depts)
-						let children = [];
-						for (let j = 0; j < item['depts'][i]['classes'].length; j++) {
-							children.push({
-								classesId: item['depts'][i]['classes'][j].id,
-								classesname: item['depts'][i]['classes'][j].name,
-								uuid: this.guid2()
-							})
-						}
-						depts[i].children = children
-					}
-					return {
-						schoolId: item.id,
-						name: item.name,
-						children: depts,
-						uuid: this.guid2()
-					}
-				})
-			}
-		}
-	}
+      console.log('Submitted:', this.dialogType, this.form);
+      this.form = {schoolId: '', deptId: '', name: ''};
+    },
+    handleDelete(a, b) {
+      console.log(b);
+      if (b.schoolId != null) {
+        this.$del("/school/" + b.schoolId)
+            .then((res) => {
+              this.$notifyMsg("成功", res.data.msg, "success")
+              this.fetchSchools();
+            })
+      } else if (b.deptsId != null) {
+        this.$del("/dept/" + b.deptsId)
+            .then((res) => {
+              this.$notifyMsg("成功", res.data.msg, "success")
+              this.fetchSchools();
+            })
+      } else {
+        this.$del("/class/" + b.classesId)
+            .then((res) => {
+              this.$notifyMsg("成功", res.data.msg, "success")
+              this.fetchSchools();
+            })
+      }
+    },
+  },
+  created() {
+    this.fetchSchools();
+  },
+  computed: {
+    dataList() {
+      return this.schools.map(item => {
+        let depts = [];
+        for (let i = 0; i < item['depts'].length; i++) {
+          // console.log(item['depts'][i]['classes'])
+          depts.push({
+            deptsId: item['depts'][i].id,
+            deptname: item['depts'][i].name,
+            uuid: this.guid2()
+          })
+          // console.log(depts)
+          let children = [];
+          for (let j = 0; j < item['depts'][i]['classes'].length; j++) {
+            children.push({
+              classesId: item['depts'][i]['classes'][j].id,
+              classesname: item['depts'][i]['classes'][j].name,
+              uuid: this.guid2()
+            })
+          }
+          depts[i].children = children
+        }
+        return {
+          schoolId: item.id,
+          name: item.name,
+          children: depts,
+          uuid: this.guid2()
+        }
+      })
+    }
+  }
+}
 </script>
 
-<style scoped lang="less">
-	.content {
-		padding: 0 1%;
-	}
+<style scoped>
+.content {
+  padding: 0 1%;
+}
+
+.dialog-footer {
+  text-align: right;
+}
 </style>
